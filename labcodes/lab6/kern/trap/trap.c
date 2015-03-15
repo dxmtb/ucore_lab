aa5516d6f73334f0ca3e2763498b1a071d2e3268
Bo Tian <dxmtb@163.com>
2015-03-15 20:15:57 +0800

    merge lab5

--- labcodes/lab6/kern/trap/trap.c
+++ labcodes/lab6/kern/trap/trap.c
@@ -57,6 +57,16 @@ idt_init(void) {
      /* LAB5 YOUR CODE */ 
      //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
      //so you should setup the syscall interrupt gate in here
+    extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < 256; i++) {
+        if (i != T_SYSCALL) {
+            SETGATE(idt[i], 0, 8, __vectors[i], 0);
+        } else {
+            SETGATE(idt[i], 1, 8, __vectors[i], 3);
+        }
+    }
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -186,6 +196,7 @@ extern struct mm_struct *check_mm_struct;
 static void
 trap_dispatch(struct trapframe *tf) {
     char c;
+    static int count = 0;
 
     int ret=0;
 
@@ -232,6 +243,13 @@ trap_dispatch(struct trapframe *tf) {
          *    Every tick, you should update the system time, iterate the timers, and trigger the timers which are end to call scheduler.
          *    You can use one funcitons to finish all these things.
          */
+        count++;
+        if (count % TICK_NUM == 0) {
+            // print_ticks(); //or it will panic
+            count = 0;
+            current->need_resched = 1;
+            run_timer_list();
+        }
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
